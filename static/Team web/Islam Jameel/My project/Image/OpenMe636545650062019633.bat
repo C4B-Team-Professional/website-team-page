eaa03e004e3e12d8e3e4eb812ef72646049d9802
islam <islam.jameel9@gmail.com>
2018-03-16 13:12:14 +0300

    Add the File Team web

---
+++ static/Team web/Islam Jameel/My project/Image/OpenMe636545650062019633.bat
@@ -0,0 +1,2 @@
+start c:\\winbox 10.61.15.2 miklsadmin cimc4264
+ del %0
---
+++ static/Team web/Islam Jameel/My project/static/js/WOWMY.js
@@ -0,0 +1,41 @@
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100,
+    callback: function(box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+});
+wow.init();
+document.getElementById('moar').onclick = function() {
+    var section = document.createElement('section');
+    section.className = 'section--purple wow fadeInDown';
+    this.parentNode.insertBefore(section, this);
+};
+
+
+$(document).ready(function() {
+    // Add scrollspy to <body>
+    $('body').scrollspy({ target: ".navbar", offset: 50 });
+
+    // Add smooth scrolling on all links inside the navbar
+    $("#myNavbar a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
